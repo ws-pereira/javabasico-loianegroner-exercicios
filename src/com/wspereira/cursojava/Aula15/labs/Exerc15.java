1b26d1194cb3509cbaac90cfdf0ac8718939567c
ws-pereira <ws-pereira@hotmail.com>
2020-12-10 23:55:07 -0300

    Alterando o loop para while, trabalhando com Fibonacci até 500

--- src/com/wspereira/cursojava/Aula15/labs/Exerc15.java
+++ src/com/wspereira/cursojava/Aula15/labs/Exerc15.java
@@ -11,35 +11,28 @@ public class Exerc15 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Entre com n-enésimo termo de Fibonacci ");
-		int nEnesimo = sc.nextInt();
-		
 		int pTermo = 0;    // Primeiro termo
 		int sTermo = 1;    // Segundo Termo
 		int fibo = 0;
 		int count = 0;
 		
 		System.out.println();
-		System.out.println("Termos de Fibonacci até " + nEnesimo);
+		System.out.println("Termos de Fibonacci até 500");
 		System.out.print(pTermo + " -> " + sTermo);
-		for(int x = 3; x <= nEnesimo; x++) {
+		while(fibo <= 500) {
 			
 			fibo = pTermo + sTermo;
 			pTermo = sTermo;
 			sTermo = fibo;
 			System.out.print(" -> " + fibo);
 			
+			// Gerando quebra de linha
 			count++;
 			if(count == 10) {
 				count = 0;
 				System.out.println();
 			}
 		}
-		
-		
-		sc.close();
 
 	}
 
