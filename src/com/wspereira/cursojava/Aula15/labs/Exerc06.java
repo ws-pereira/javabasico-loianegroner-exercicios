f2bd6279da8c237279dc4f17f4b2550b954e4bab
ws-pereira <ws-pereira@hotmail.com>
2020-12-10 21:24:52 -0300

    Exercício simples de impressão de nros

---
+++ src/com/wspereira/cursojava/Aula15/labs/Exerc06.java
@@ -0,0 +1,22 @@
+/* Autor: Wanderson Pereira
+ * Exercícios curso Java Básico - Instrutora Loiane Groner
+ * Trabalhando com loop - imprimindo nros de 1 - 20
+ */
+
+package com.wspereira.cursojava.Aula15.labs;
+
+public class Exerc06 {
+
+	public static void main(String[] args) {
+		
+		for(int x = 1; x <=20; x++) {
+			System.out.println(x);
+		}
+		
+		for(int x = 1; x<=20; x++) {
+			System.out.print(x + " ");
+		}
+		
+	}
+
+}
