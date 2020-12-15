263807d7e174f33e5e8b1bbdd66b574ca552fdc3
ws-pereira <ws-pereira@hotmail.com>
2020-12-14 23:22:57 -0300

    Exercício de simulação de uma caixa registradora antiga

---
+++ src/com/wspereira/cursojava/Aula15/labs/Exerc25.java
@@ -0,0 +1,40 @@
+/* Autor: Wanderson Pereira
+ * Exercícios curso Java Básico - Instrutora Loiane Groner
+ * Trabalhando com loop - apresentar o valor de 1 até 50, a partir de um preço
+ */
+
+package com.wspereira.cursojava.Aula15.labs;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exerc25 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Lojas Tabajaras");
+		
+		double somaValor = 0.00;
+		double valorProduto = 0;
+		int x = 1;
+		
+		do {
+			System.out.print("Produto " + x + " R$ ");
+			valorProduto = sc.nextDouble();
+			somaValor += valorProduto;
+			x++;
+		}while(valorProduto != 0);
+		
+		System.out.println("Total - R$ " + String.format("%.2f", somaValor));
+		System.out.print("Dinheiro - R$ ");
+		double pgtoDinheiro = sc.nextDouble();
+		double troco = pgtoDinheiro - somaValor;
+		
+		System.out.println("Troco - R$ " + String.format("%.2f", troco));
+		sc.close();
+	}
+
+}
